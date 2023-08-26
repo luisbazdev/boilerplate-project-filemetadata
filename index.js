4fb41a1827319099524c66b95e06578c1828d186
Luis Barraza <103273973+luisbazdev@users.noreply.github.com>
2023-08-25 21:33:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+require("dotenv").config();
 
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+var multer = require("multer");
+var upload = multer();
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single('upfile'), function(req, res) {
+  const name = req.file.originalname;
+  const type = req.file.mimetype;
+  const { size } = req.file
+  res.json({name, type, size})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
